25334107c2cf5ee6e3c801f5912211688cff94ca
Amábile <amabile.analistadesistemas@gmail.com>
2023-11-28 16:17:27 -0300

    feat: create functions

--- src/App.js
+++ src/App.js
@@ -5,4 +5,13 @@ import ExpenseList from './ExpenseList';
 import IncomeForm from './IncomeForm';
 import IncomeList from './IncomeList';
 
+function App() {
+  // State para guardar despesas e receitas
+  const [expenses, setExpenses] = useState([]); // Array para despesas
+  const [incomes, setIncomes] = useState([]); // Array para receitas
+
+  // Funções para adicionar despesas e receitas ao estado correspondente
+  const addExpense = (expense) => {
+    setExpenses([...expenses, expense]); // Adiciona uma nova despesa ao array de despesas
+  };
 
